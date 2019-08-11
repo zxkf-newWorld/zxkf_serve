9f32b1fcec5c5e9b339bc87713b14777d67e105f
Hepeng <hepeng@baba.com>
2019-08-11 10:57:37 +0800

    zxkf-serve项目文件的结构建立

---
+++ app.js
@@ -0,0 +1,46 @@
+//  1:加载第三方模块
+const express = require("express");
+const mysql = require("mysql");
+const cors = require("cors");
+const session = require("express-session");
+
+// 2：配置数据库连接池
+var pool = mysql.createPool({
+    host:"127.0.0.1",
+    user:"root",
+    password:"",
+    port:3306,
+    database:"xzkf",
+    connectionLimit:15
+});
+//创建服务器
+var server = express();
+// 3：配置跨域模块
+server.use(cors({
+    // 允许跨域访问程序地址列表
+    origin:["http://127.0.0.1:8080",
+            "http://localhost:8080"
+    ],
+    credentials:true,//请求验证
+}));
+// 4：配置session模块
+server.use(session({
+    secret:"128位字符串",//安全字符串
+    resave:true,//请求时更新数据
+    saveUninitialized: true,//保存初始数据 
+}));
+//4.1：配置项目的静态目录
+// 可以在地址后面添加所需资源（名称/或者public下的路径+资源名称）
+server.use(express.static("public")); 
+
+// 监听端口号3000
+server.listen(3000);
+// 相关接口的配置
+// 功能1：完成用户登录操作
+// 接口相关测试
+// http://127.0.0.1:3000/接口名称（路径）
+server.get("/demo",(req,res)=>{
+    console.log("IN接口");
+    // 接口代码
+    console.log("OUT接口");
+});
